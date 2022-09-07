a992ee1f404d4bcd3c2c83fc55fbf96d7b08ca19
Gaurav Paudel <gaurav.paudel@wesionary.team>
2022-09-07 23:33:45 +0545

    chore: :wrench: seperated config settings files and used dot env for env variables

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,1 +1,17 @@
 from .base import *
+
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config["DB_NAME"],
+        "USER": config["DB_USER"],
+        "PASSWORD": config["DB_PASSWORD"],
+        "HOST": config["DB_HOST"],
+        "PORT": config["DB_PORT"],
+        "CONN_MAX_AGE": 600,
+    }
+}
